23f85f821f601ab8541def96a78837eec271420e
Pragati Raut <pragati.raut@krazy-tech.com>
2025-12-25 14:05:38 +0530

    Initial Secret Santa project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,64 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
-export default function Home() {
+export default function Page() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="screen">
+      <div className="card single">
+
+        {/* SANTA — before click */}
+        {!open && (
+          <div className="left only" onClick={() => setOpen(true)}>
+            <p className="tap-text">Click to Open 🎁</p>
+
+            <div className="scene">
+              <div className="snowground"></div>
+              <div className="tree t1"></div>
+              <div className="tree t2"></div>
+
+              <div className="chimney">
+                <div className="snowcap"></div>
+                <div className="santa">
+                  <div className="belt"></div>
+                  <div className="leg left-leg"></div>
+                  <div className="leg right-leg"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {/* MESSAGE — after click */}
+        {open && (
+          <div className="right only show">
+            <h2>From Your Secret Santa</h2>
+
+            <p><b>Dear, Anmol Sir </b></p>
+
+            <p>
+             I really appreciate your helping nature. You are always supportive and kind.
+              I have learned a lot from you. You motivate me and make the workplace feel positive.
+            </p>
+
+            <p>
+              Thank you for being such a supportive and approachable person.
+              Wishing you a wonderful festive season filled with happiness,
+              peace, laughter and success.
+            </p>
+
+            <p className="ps">
+              P.S. UI still not look nice… I tried again 😄  
+              Hope this one finally made you smile 😁
+            </p>
+
+            <div className="sign">— Your Secret Santa 🎄</div>
+            <div className="date">25 December • Merry Christmas</div>
+          </div>
+        )}
+
+      </div>
     </div>
   );
 }
